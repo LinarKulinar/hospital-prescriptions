8f63f5457a2d9d529b64dbf5fb3c36a4a4397457
LinarKulinar <40643377+LinarKulinar@users.noreply.github.com>
2020-08-13 00:03:24 +0400

    Added scripts for creating / deleting / filling the database

---
+++ db/sql/create_table.sql
@@ -0,0 +1,30 @@
+CREATE TABLE PATIENT
+(
+    patient_id   BIGINT IDENTITY PRIMARY KEY,
+    first_name   VARCHAR(40) DEFAULT NULL,
+    last_name    VARCHAR(40) DEFAULT NULL,
+    patronymic   VARCHAR(40) DEFAULT NULL,
+    phone_number VARCHAR(40) DEFAULT NULL
+);
+
+CREATE TABLE DOCTOR
+(
+    doctor_id      BIGINT IDENTITY PRIMARY KEY,
+    first_name     VARCHAR(40) DEFAULT NULL,
+    last_name      VARCHAR(40) DEFAULT NULL,
+    patronymic     VARCHAR(40) DEFAULT NULL,
+    specialization VARCHAR(40) DEFAULT NULL
+);
+
+CREATE TABLE PRESCRIPTION
+(
+    prescription_id BIGINT IDENTITY PRIMARY KEY,
+    description     VARCHAR(1000),
+    patient_id      BIGINT,
+    doctor_id       BIGINT,
+    date_creat      DATE CHECK (date_creat <= current_date),
+    validity_period BIGINT,
+    priority        VARCHAR(40) CHECK (priority IN ('Нормальный', 'Cito', 'Statim')),
+    FOREIGN KEY (patient_id) REFERENCES PATIENT (patient_id),
+    FOREIGN KEY (doctor_id) REFERENCES DOCTOR (doctor_id)
+);
